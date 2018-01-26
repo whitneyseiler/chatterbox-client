1588b9fb006dc9c94efff489c17db6ae96a51d57
Whitney Seiler <whitney.seiler@gmail.com>
2018-01-26 14:34:06 -0800

    Basic Setup

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,40 @@
-// YOUR CODE HERE:
+var app = {
+  init: function(){
+    return true;
+  },
+
+  send: function(message){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.HRSF90.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      // message: {
+      //   username: 'shawndrost',
+      //   text: 'trololo',
+      //   roomname: '4chan'
+      // };
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+
+  fetch: function(){
+    $.get();
+  },
+
+  renderMessage: function() {},
+
+  renderRoom: function(){},
+
+  clearMessages: function(){
+    return true;
+  }
+
+}
